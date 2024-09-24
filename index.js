7468b94103266aa3a4f9f0322292d12ac409cd4c
jabaringure@gmail.com <jabaringure@gmail.com>
2024-09-24 16:15:11 +0300

    adds complete Arithmetics

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let multiply = 0
+let num1 = 7;
+let num2 = 7;
+multiply += 13 + (num1 * num2);
+
+let math;
+let random = 1;
+let mod = 0;
+let num3 = 16;
+let num4 = 6;
+mod = num3 % num4;
+
+const highest = new Set([5, 10, 15, 20]);
+const max = Math.max(5, 10, 15, 20)
